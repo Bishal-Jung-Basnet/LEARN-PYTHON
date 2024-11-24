59646fd37e1f23c54f0b71738bae6499986ff8f4
Bishal Jung Basnet <BishalBasnet79@ktmmodelcollege.edu.np>
2024-11-24 10:08:19 +0545

    Converts char into unicode

---
+++ ORD_function.py
@@ -0,0 +1,6 @@
+chara = 'a'
+charA ='A'
+unicodeValuea = ord(chara)
+unicodeValueA = ord(charA)
+print(f"The unicode of '{chara}' is {unicodeValuea}")
+print(f"The unicode of '{charA}' is {unicodeValueA}")
