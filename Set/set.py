e13a6af295d5b064c8ffe46364ba90a0c434c1b5
Bishal Jung Basnet <BishalBasnet79@ktmmodelcollege.edu.np>
2024-12-27 08:22:41 +0545

    set made

---
+++ Set/set.py
@@ -0,0 +1,8 @@
+student_id = {101,102,103,104,105}
+print('student_id:', student_id)
+
+vowelLetters={'a','e','i','o','u'}
+print('Vowel Letters:', vowelLetters)
+
+mixedSet={'Hello', 101, -199,'Bye'}
+print("Set of mixed data types:", mixedSet)
