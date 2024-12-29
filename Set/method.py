659eb8d5a46e7bf38ff0cc0469654ff808007543
Bishal Jung Basnet <BishalBasnet79@ktmmodelcollege.edu.np>
2024-12-29 10:09:24 +0545

    intersection

--- Set/method.py
+++ Set/method.py
@@ -24,7 +24,12 @@ for fruit in fruits:
 print(len(fruits))
 
 A = {1,3,5}
-B = {0,2,4}
+B = {0,1,2,4}
 #union operation using |
 print("Union  using | :",A|B)
 print("Union using union():", A.union(B))
+
+#perform intersection using &
+print("Untersection using &: ",A&B)
+#intersection using intersection()
+print("Untersection using intersection(): ",A.intersection(B))
