09a2f4c94f28dad02180d47d96747b2b149875af
Bishal-Jung-Basnet <bishal.jung.basnet.2024@midvalley.edu.np>
2025-01-21 10:04:48 +0545

    minor chnages

---
+++ Tutorial/Tut04-01.py
@@ -0,0 +1,6 @@
+studentId = input("Enter student id: ")
+tutorial = input("Enter the marks for tutorial: ")
+assig1 = input("Enter the marks for assignment 1 : ")
+assig2 = input("Enter the marks for assignment 2: ")
+exam = input("Enter marks for exam: ")
+total = tutorial + assig1 + assig2 + exam
